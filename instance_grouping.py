10977967b9b73e0cf8b38429eca7ae5500a5920e
Diego <diego.bengocheapaz@pm.me>
2023-01-02 17:16:19 +0100

    Drafts of future modules in a geolearn package.

---
+++ instance_grouping.py
@@ -0,0 +1,23 @@
+"""
+This module includes functions used to group instances by spatial proximity and to aggregate feature values within groups of instances.
+Two grouping methods are provided: 
+    1- Intersecting buffers.
+    2- Grid tiles.
+Dataset instances are rows of a Point GeoDataFrame.
+
+Created on Monday January 02 2023.
+@author: Diego Bengochea Paz.
+"""
+
+
+def group_by_intersecting_buffers():
+    """
+    """  
+
+def group_by_tiling():
+    """
+    """
+
+def aggregate_values_within_group():
+    """
+    """
