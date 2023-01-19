308c5bcf7e2150f7b909161e9f4aed7acecb44c0
Diego <diego.bengocheapaz@pm.me>
2023-01-19 17:55:07 +0100

    progress in reflecting border conditions

--- map_comparison_structural_similarity.py
+++ map_comparison_structural_similarity.py
@@ -12,9 +12,13 @@ Production of new maps with:
 """
 
 import rasterio
+from rasterio.windows import Window
+from scipy.ndimage import gaussian_filter
+
 import numpy as np
 import pandas as pd
 import geopandas as gpd
+
 
 
 # Paths to the two rasters to compare 
@@ -81,7 +85,6 @@ def window_mean(array, weights):
     return np.sum(weights*array)
 
 
-
 def window_variance(array, mean, weights):
     """
     :param array: the numpy array corresponding to the raster.
@@ -109,9 +112,94 @@ def window_covariance(array1,array2,mean1,mean2,weights):
     return np.sum(weights * diff1 * diff2)
 
 # Window creation 
+def reflect_border(raster,row,col,d_height,d_width,side_length):
+    height = side_length - np.floor(side_length*0.5) + d_height
+    width = side_length - np.floor(side_length*0.5) + d_width 
+    
+    row_origin = 
+    col_origin = 
+    
+    window = create_window(raster,col_origin,row_origin,height,width)    
+    
+    reflection = np.zeros((side_length,side_length))
+    for row in range(side_length):
+        for col in range(side_length):
+            i = np.int(np.abs(row - np.floor(side_length*0.5)))
+            j = np.int(np.abs(col - np.floor(side_length*0.5)))
+            reflection[row,col] = window[i,j]
 
-def create_windows():
-    return
+    
+    return 
 
-def weights():
-    return
+def create_window(raster,col,row,width,height):
+    return raster.read(1, window=Window(col,row,width,height))
+    
+def weights(side_length):
+    weights = np.ones((side_length,side_length))
+    std = weights.size*0.3333333
+    return gaussian_filter(weights,sigma=std)
+
+def value_range(array1,array2):
+    max = np.max(np.array([array1,array2]))
+    min = np.mix(np.array([array1,array2]))
+    return max-min
+
+# Window statistics
+
+def window_sim(window1,window2,val_range,weights):
+    mean1 = window_mean(window1,weights)
+    mean2 = window_mean(window2,weights)
+    return similarity_in_mean(mean1,mean2,val_range)
+
+def window_siv(window1,window2,val_range,weights):
+    mean1 = window_mean(window1,weights)
+    mean2 = window_mean(window2,weights)
+    var1 = window_variance(window1,mean1,weights)
+    var2 = window_variance(window2,mean2,weights)
+    return similarity_in_variance(var1,var2,val_range)
+
+def window_sip(window1,window2,val_range,weights):
+    mean1 = window_mean(window1,weights)
+    mean2 = window_mean(window2,weights)
+    cov = window_covariance(window1,window2,mean1,mean2,weights)
+    var1 = window_variance(window1,mean1,weights)
+    var2 = window_variance(window2,mean2,weights)
+    return similarity_in_pattern(var1,var2,cov,val_range)
+
+    
+# Map comparison
+
+def map_index(raster1,raster2,side_length,index):
+    with rasterio.open(raster1) as src1:
+        with rasterio.open(raster2) as src2:
+            nrows = src1.height
+            ncols = src1.width
+   
+            index_map = np.zeros((nrows-side_length,ncols-side_length))
+
+            for row in range(nrows-side_length):
+                for col in range(ncols-side_length):
+                    window1 = create_window(src1,col,row,side_length,side_length)
+                    window2 = create_window(src2,col,row,side_length,side_length)
+                    
+                    val_range = value_range(window1,window2)
+                    weights = weights(side_length)
+
+                    metric = index(window1,window2,val_range,weights)
+
+                    index_map[row,col] = metric
+
+    return index_map    
+
+def map_sim(raster1,raster2,side_length):
+    return map_index(raster1,raster2,side_length,window_sim)
+
+def map_siv(raster1,raster2,side_length):
+    return map_index(raster1,raster2,side_length,window_siv)       
+
+def map_sip(raster1,raster2,side_length):
+    return map_index(raster1,raster2,side_length,window_sip)       
+
+
+            
+            
